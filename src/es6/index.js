b8340c34d90ed6f43b428c578ee0912eadc41351
romipaz <eccoromi@gmail.com>
2020-06-26 10:29:21 +0200

    add content es6

--- src/es6/index.js
+++ src/es6/index.js
@@ -14,7 +14,7 @@ function newFunction2 (name = 'Megan', age = 20, country = 'United States') {
     console.log(name, age, country);
 }
 
-newFunction2() // Output default values
+newFunction2() // Outputs default values
 newFunction2('Megan E.', 20, 'USA');
 
 // Template literals before ECMAScript 6
@@ -29,3 +29,48 @@ console.log(greeting);
 
 let greeting2 = `${hello} ${world}`;
 console.log(greeting2);
+
+//Multiline in es6
+let lorem = `Uncle Sam
+WANTS YOU`;
+
+console.log(lorem);
+
+//Destructuring in es6
+let person = {
+    "name": "Mads",
+    "age": 32,
+    "country": "DK"
+}
+
+console.log(person.name, person.age);
+
+let { name, age, country} = person; //same as upper console.log 
+console.log(name);
+
+//Spread Operator in es6
+let team1 = ["Sara", "Toby", "Nina"];
+let team2 = ["Juan", "Mikkel", "Agos"];
+
+let education = ["Ale", ...team1, ...team2];//Call for elements within an array
+
+console.log(education);
+
+//Scoping
+
+var hola = "hola" //global scope
+
+{
+    var global = "Global Var";
+}
+
+{
+    let globalLet = "Global let"; //block scope
+    console.log(globalLet);
+}
+
+console.log(global);
+
+const a = "b";
+a = "a";
+console.log(a);
