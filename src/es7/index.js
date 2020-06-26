0d949b5bba6053be8343cf086f1cea7d5145609a
romipaz <eccoromi@gmail.com>
2020-06-26 10:48:54 +0200

    add es7 and es8

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+// Includes
+let numbers = [1, 2, 3, 7, 8];
+constVALUE = 7;
+
+if (numbers.includes(VALUE)) {
+  console.log(`${VALUE} is found`);
+} else {
+  console.log(`${VALUE} cannot be found`);
+}
+
+// Pow
+let base = 4;
+let exponent = 4;
+letresult = base ** exponent;
+console.log(`Result -> ${result}`);
---
+++ src/es8/index.js
@@ -0,0 +1,34 @@
+//Object entries returns the values of the array.
+
+const data ={
+    front:'Alej',
+    back: 'Rel'
+};
+
+//Transform object into an array
+const entries =Object.entries(data);
+console.log(entries);
+//length to check the quantity of elements within an array
+console.log(entries.length);
+
+
+
+//Objetc Values: return the object values as array elements. 
+const data= {
+    front:'Alej',
+    back: 'Rel'
+}
+const values = Object.values(data);
+console.log(values);
+
+// Padding: adds characters or empty space into a string.
+conststring ='hello';
+console.log(string.padStart(7,'hi')) // adds 'hi' at the start
+console.log(string.padEnd(12,'hi')) // adds 'hi' at the end
+
+
+//Trailing comas,to keep assigning elements into the object
+const data= {
+    front:'Alej', // Puede existir
+    back: 'Rel'
+} 
