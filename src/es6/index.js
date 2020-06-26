6655d852d0ad435291dd8c3e3eb7c112aa429f47
romipaz <eccoromi@gmail.com>
2020-06-26 10:35:25 +0200

    add content es6

--- src/es6/index.js
+++ src/es6/index.js
@@ -74,3 +74,85 @@ console.log(global);
 const a = "b";
 a = "a";
 console.log(a);
+
+
+// Parameters within objects
+// Before es6
+let obj = { name: name, age: age }
+
+// After es6
+let obj2 = { name, age}
+
+console.log(obj)
+console.log(obj2)
+
+// Arrow Functions (new add-on)
+const names = [
+  { name: 'Mich', age: 35 },
+  { name: 'Yesica', age: 38}
+]
+
+//Before
+let items = names.map( function(item) {
+  console.log(item.name + ' - ' + item.age)
+})
+
+//ES6
+let items2 = names.map( item => console.log(`${item.name} ~ ${item.age}`) )
+
+console.log(items)
+console.log(items2)
+
+// Promises (new add-on)
+const helloPromise = () => {
+  return new Promise((resolve, reject) => {
+    if (true) {
+      resolve('Hey !!!')
+    } else {
+      reject('Upsss!!! :(')
+    }
+  })
+}
+
+helloPromise()
+  .then( response => console.log(response) )
+  .then( ()=> console.log('Hola'))
+  .catch( error => console.error(error) )
+
+
+// Classes
+classcalculator{
+    constructor() {
+      this.valueA = 0
+      this.valueB = 0
+    }
+    sum(valueA, valueB) {
+      return valueA + valueB
+    }
+  }
+  
+  const calc = new calculator()
+  
+  console.log(calc.sum(4,5))
+  
+  
+// Modules 
+  import { hello } from'./module'
+  
+  hello()
+  
+  
+// Generators
+  function* helloworld () {
+    if (true) {
+      yield'Hello'
+    }
+    if (true) {
+      yield'World'
+    }
+  }
+  
+  const generatorHello = helloworld()
+  console.log(generatorHello.next().value)
+  console.log(generatorHello.next().value)
+  console.log(generatorHello.next().value)
